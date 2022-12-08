9d09735e96e75e5bccd7958fb5bfc3fb4d5f8830
abylayomar <abylay.omar@gmail.com>
2022-12-09 00:04:51 +0600

    create answers instances

---
+++ app/Models/Answer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'text',
+    ];
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
+}
