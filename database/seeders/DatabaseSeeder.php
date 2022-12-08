8030db571d7cb0b1ed7cc0b433a7d6fbffbe8a2a
abylayomar <abylay.omar@gmail.com>
2022-12-09 00:07:53 +0600

    create respondents

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Question::factory(15)->create();
+        \App\Models\Answer::factory(60)->create();
+
+        $this->call([
+            QuestionAnswerSeeder::class,
+        ]);
     }
 }
